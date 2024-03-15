9b4933743d2ea453acfce107dacf33ba9b00de50
Developer <developer@Developers-Mac-mini.local>
2024-03-15 13:10:42 +0530

    add solace publisher

---
+++ src/main/java/one/ozone/www/SolacePublisherApplication.java
@@ -0,0 +1,13 @@
+package one.ozone.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolacePublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolacePublisherApplication.class, args);
+	}
+
+}
